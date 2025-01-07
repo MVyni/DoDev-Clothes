98a9ffb3e06dfca952a7b4293057da46ab77b00b
Marcus Vynicius <viniciusfla1999@gmail.com>
2025-01-07 22:41:40 +0000

    iniciando interatividade no menu

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 function clickMenu() {
     const itens = document.getElementById('itens');
 
-    if (itens.style.display === 'block'){
-        itens.style.display = 'none';
-    } else {
+    if (itens.style.display === 'none'){
         itens.style.display = 'block';
+    } else {
+        itens.style.display = 'none';
     }
+}
+
+function menuClick() {
+    const menuBurger = document.getElementById('menuBurger');
+
+    menuBurger.addEventListener('click', (event) => {event.style.display = 'block'})
 }
