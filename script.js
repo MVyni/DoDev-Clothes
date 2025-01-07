2b787d260d771538cd1be73d8c787300c64222d9
Marcus Vynicius <viniciusfla1999@gmail.com>
2025-01-07 21:54:33 +0000

    terminando de estilizar e iniciando o js

---
+++ script.js
@@ -0,0 +1,9 @@
+function clickMenu() {
+    const itens = document.getElementById('itens');
+
+    if (itens.style.display === 'block'){
+        itens.style.display = 'none';
+    } else {
+        itens.style.display = 'block';
+    }
+}
