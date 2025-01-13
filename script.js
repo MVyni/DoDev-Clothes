377e2e2536fe3be5370eb1ee6a4d3740b2d4ca8c
Marcus Vynicius <viniciusfla1999@gmail.com>
2025-01-13 22:48:43 +0000

    começando a configurar menu bar

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
+const mobileMenu = document.querySelector(".mobile-menu");
+const menuBurger = document.querySelector(".menuBurger")
+
+menuBurger.addEventListener("click", () => {
+    mobileMenu.classList.add("ativo");
+})
+
+
+
+
+
+
 
 function menuClick() {
-    const menuBurger = document.getElementById('menuBurger');
-    const itens = document.getElementById('itens');
+    const itens = document.querySelector('.mobile-menu');
 
-    menuBurger.addEventListener('click', () => {
     if(itens.style.display === "none"){
         itens.style.display = "block";
     } else {
         itens.style.display = "none";
         }
-    })
 }
