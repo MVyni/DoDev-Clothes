2147ff81b6a7823fbe6255c822c92ced1b447efa
Marcus Vynicius <viniciusfla1999@gmail.com>
2025-01-14 23:02:53 +0000

    finalizando o menu burger na home

--- script.js
+++ script.js
@@ -1,22 +1,8 @@
 const mobileMenu = document.querySelector(".mobile-menu");
-const menuBurger = document.querySelector(".menuBurger")
+const menuBurger = document.querySelector(".menuBurger");
 
 menuBurger.addEventListener("click", () => {
-    mobileMenu.classList.add("ativo");
-})
 
-
-
-
-
-
-
-function menuClick() {
-    const itens = document.querySelector('.mobile-menu');
-
-    if(itens.style.display === "none"){
-        itens.style.display = "block";
-    } else {
-        itens.style.display = "none";
-        }
-}
+  mobileMenu.classList.toggle("ativo");
+  menuBurger.classList.toggle("ativo");
+});
