2f4f54f124d1d2cf6216354bdbeeb7dc5d8e0fc2
Marcus Vynicius <viniciusfla1999@gmail.com>
2025-01-21 23:11:51 +0000

    aplicando js na troca de imagem na página produtos

--- script.js
+++ script.js
@@ -6,86 +6,100 @@ const sobreNomeForm = document.getElementById("sobreNome");
 const emailForm = document.getElementById("email");
 const telForm = document.getElementById("tel");
 const form = document.getElementById("form");
-
+const roupas = document.querySelector(".roupa02");
+const roupa1 = document.querySelector("#roupa1");
+const roupa2 = document.querySelector("#roupa2");
+const roupa3 = document.querySelector("#roupa3");
 
 menuBurger.addEventListener("click", () => {
-
   mobileMenu.classList.toggle("ativo");
   menuBurger.classList.toggle("ativo");
 });
 
 //SALVANDO FORM NO LOCAL STORAGE
-function saveName(text){
-  window.sessionStorage.setItem('nome', text)
+function saveName(text) {
+  window.sessionStorage.setItem("nome", text);
 }
 
-nomeForm.addEventListener('blur', (e) => {
+nomeForm.addEventListener("blur", (e) => {
   saveName(e.target.value);
-})
+});
 
-
-function saveSobreNome(text){
-  window.sessionStorage.setItem('sobrenome', text)
+function saveSobreNome(text) {
+  window.sessionStorage.setItem("sobrenome", text);
 }
 
-sobreNomeForm.addEventListener('blur', (e) => {
+sobreNomeForm.addEventListener("blur", (e) => {
   saveSobreNome(e.target.value);
-})
+});
 
-
-function saveEmail(email){
-  window.sessionStorage.setItem('e-mail', email)
+function saveEmail(email) {
+  window.sessionStorage.setItem("e-mail", email);
 }
 
-emailForm.addEventListener('blur', (e) => {
+emailForm.addEventListener("blur", (e) => {
   saveEmail(e.target.value);
-})
+});
 
-
-function saveTel(tel){
-  window.sessionStorage.setItem('telefone', tel)
+function saveTel(tel) {
+  window.sessionStorage.setItem("telefone", tel);
 }
 
-telForm.addEventListener('blur', (e) => {
+telForm.addEventListener("blur", (e) => {
   saveTel(e.target.value);
-})
-
+});
 
 //ENVIANDO FORM PARA API
-async function postForm(url = "https://apigenerator.dronahq.com/api/kfuTuMtf/form", data = "{}"){
-
-  const response = await fetch("https://apigenerator.dronahq.com/api/kfuTuMtf/form", {
-    method: "POST",
-    mode: "cors",
-    cache: "no-cache",
-    credentials: "same-origin",
-    headers: {
-      "Content-Type": "application/json"},
+async function postForm(
+  url = "https://apigenerator.dronahq.com/api/kfuTuMtf/form",
+  data = "{}"
+) {
+  const response = await fetch(
+    "https://apigenerator.dronahq.com/api/kfuTuMtf/form",
+    {
+      method: "POST",
+      mode: "cors",
+      cache: "no-cache",
+      credentials: "same-origin",
+      headers: {
+        "Content-Type": "application/json",
+      },
       redirect: "follow",
       referrerPolicy: "no-referrer",
-      body: JSON.stringify(data)
-  });
-  return response.json()
+      body: JSON.stringify(data),
+    }
+  );
+  return response.json();
 }
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  
-  const formulario = e.currentTarget
+
+  const formulario = e.currentTarget;
   const data = {
-    "Nome": formulario.nome.value,
-    "Sobrenome": formulario.sobreNome.value,
-    "Email": formulario.email.value,
-    "Telefone": formulario.tel.value
-  }
+    Nome: formulario.nome.value,
+    Sobrenome: formulario.sobreNome.value,
+    Email: formulario.email.value,
+    Telefone: formulario.tel.value,
+  };
 
   postForm("https://apigenerator.dronahq.com/api/kfuTuMtf/form", data)
-  .then((data) =>{
-    alert("Formulário enviado com sucesso");
-    
-    formulario.reset();
-  })
-  .catch((data) => {
-    console.log("Erro ao enviar:", data)  
-  })
-})
+    .then((data) => {
+      alert("Formulário enviado com sucesso");
+
+      formulario.reset();
+    })
+    .catch((data) => {
+      console.log("Erro ao enviar:", data);
+    });
+});
+
+//MUDANDO IMAGEM NA PÁGINA PRODUTOS
+function changeImg() {
+  roupa2.addEventListener("click", () => {
+    let srcFloat = roupa1.src;
+
+    roupa1.src = roupa2.src;
+    roupa2.src = srcFloat;
+  });
+}
